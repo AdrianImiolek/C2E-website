383c3f6100669a3c149a19cf6df7a20597492d9d
Adek <aaadinixxx@gmail.com>
2025-03-23 16:08:04 +0100

    It's a test branch

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('.nav-mobile');
+const navShadow = document.querySelector('.nav-shadow');
+const navBtn = document.querySelector('.nav-button');
+const allNavLinks = document.querySelectorAll('.nav__link');
+const footerCurrentDate = document.querySelector('.current-date');
+
+const toggleMenu = () => {
+    navShadow.classList.toggle("nav-shadow-active");
+    nav.classList.toggle("nav-active");
+    allNavLinks.forEach((item) => {
+        item.addEventListener("click", () => {
+            nav.classList.remove("nav-active");
+            navShadow.classList.remove("nav-shadow-active");
+        })
+    }) 
+}
+
+const handleYear = () => {
+    const year = new Date().getFullYear();
+    footerCurrentDate.textContent = year;
+}
+
+handleYear();
+navBtn.addEventListener('click',toggleMenu);
